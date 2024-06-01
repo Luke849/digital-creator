f5608640b7add2ab4c0b0b769c400a1e81001f90
Luke849 <luke.lagler@bluewin.ch>
2024-06-01 13:42:31 +0200

    email

--- app/api/waitlist/route.ts
+++ app/api/waitlist/route.ts
@@ -1,7 +1,8 @@
 import { EmailTemplate } from "@/components/component/email-template";
 import { createClient } from "@/utils/supabase/client";
 import { NextResponse } from "next/server";
-import { Resend } from 'resend';
+import { mailOptions, transporter } from '@/config/nodemailer';
+
 
 
 async function saveEmailToWaitlist(email: string) {
@@ -26,21 +27,15 @@ async function saveEmailToWaitlist(email: string) {
 
 
 async function sendEmail(email: string) {
-
-const resend = new Resend(process.env.RESEND_API_KEY);
   try {
-    const { data, error } = await resend.emails.send({
-      from: 'Digital Creator <waitlist@digital-creator.vercel>',
-      to: [`${email}`],
-      subject: 'Welcome to the Waitlist of Digtial Creator',
-      react: EmailTemplate(),
-    });
+    await transporter.sendMail({
+      ...mailOptions,
+      to: email,
+      subject: "Welcome to the Waitlist of Digtial Creator",
+      html: "<h1>HTML</h1>"
+    })
 
-    if (error) {
-      return { success: false, message: 'Internal Server Error' };
-    }
-
-    return { success: true, message: 'Email added to waitlist successfully' };
+    return { success: true, message: 'Sent welcome email successfully' };
   } catch (error) {
     return { success: false, message: 'Internal Server Error' };
   }
@@ -55,13 +50,12 @@ export async function POST(request: Request) {
   }
 
   const saveEmailToWaitlistResult = await saveEmailToWaitlist(email);
-  const sendEmailResult = await sendEmail(email);
-
-
+  const senWelcomeEmail = await sendEmail(email);
+  
   if (saveEmailToWaitlistResult.success) {
     return NextResponse.json({ message: saveEmailToWaitlistResult.message }, { status: 200 });
-  } else if (sendEmailResult.success) {
-    return NextResponse.json({ message: sendEmailResult.message }, { status: 200 });
+  } else if (!senWelcomeEmail.success) {
+    return NextResponse.json({ message: senWelcomeEmail.message }, { status: 500 });
   } else {
     return NextResponse.json({ error: saveEmailToWaitlistResult.message }, { status: 500 });
   }
